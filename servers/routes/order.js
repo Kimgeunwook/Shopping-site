34d02a5ac20b4777ac6f2af3dd88cc8d4fdf0b98
geunwook <wook961206@naver.com>
2020-07-22 18:04:52 +0900

    oreder페이지에서 서버와 통신

--- servers/routes/order.js
+++ servers/routes/order.js
@@ -1,6 +1,14 @@
 module.exports = function(app, User){//함수로 만들어 객체 app을 전달받음
+    const Order = require('../models/order');
 	var express = require('express');
     var router = express.Router();
-    
-return router;	//라우터를 리턴
+    router.get('/', function (req, res) {
+        console.log('여기들어옴')
+        Order.find({name : '카라티'})
+        .then(ord =>{
+            res.send(ord)
+        })
+       
+	})
+    return router;	//라우터를 리턴
 };
--- src/Orders.js
+++ src/Orders.js
@@ -1,4 +1,101 @@
-import React from 'react';
+// import React from 'react';
+// import Link from '@material-ui/core/Link';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Table from '@material-ui/core/Table';
+// import TableBody from '@material-ui/core/TableBody';
+// import TableCell from '@material-ui/core/TableCell';
+// import TableHead from '@material-ui/core/TableHead';
+// import TableRow from '@material-ui/core/TableRow';
+// import Title from './Title';
+// import RowList from './RowList.js';
+// import Search from './Search';
+// import PageControl from './PageControl';
+// // Generate Order Data
+// function createData(check, id, type, date , num, product, seller, orderer,price, status) {
+//   return { check, id, type, date , num, product, seller, orderer, price, status };
+// }
+
+// const rows = [
+//   createData('true', 0, '교환', '2020-07-13', '201511178', 'zara 티셔츠', 'ZARA', '근욱(yuk888)', '30,000원', '배송중'),
+//   createData('true', 1, '교환', '2020-07-13', '201511178', 'zara 티셔츠', 'ZARA', '근욱(yuk888)', '30,000원', '배송중'),
+//   createData('true', 2, '교환', '2020-07-13', '201511178', 'zara 티셔츠', 'ZARA', '근욱(yuk888)', '30,000원', '배송중'),
+//   createData('true', 3, '교환', '2020-07-13', '201511178', 'zara 티셔츠', 'ZARA', '근욱(yuk888)', '30,000원', '배송중'),
+//   createData('true', 4, '교환', '2020-07-13', '201511178', 'zara 티셔츠', 'ZARA', '근욱(yuk888)', '30,000원', '배송중'),
+// ];
+
+// function preventDefault(event) {
+//   event.preventDefault();
+// }
+
+// const useStyles = makeStyles((theme) => ({
+//   seeMore: {
+//     marginTop: theme.spacing(3),
+//   },
+//   tablehead : {
+//     background : 'lightgray',
+//   },
+//   page : {
+//     display: 'flex',
+//     marginTop : theme.spacing(3),
+//     justifyContent: 'center',
+//   }
+// }));
+
+// export default function Orders() {
+//   const classes = useStyles();
+//   return (
+//     <React.Fragment >
+//       <Title >주문 현황</Title>
+//       <Search />
+//       <RowList/>
+
+      
+//       <Table size="small">
+//         <TableHead className={classes.tablehead}>
+//           <TableRow>
+//             <TableCell>선택</TableCell>
+//             <TableCell>번호</TableCell>
+//             <TableCell>유형</TableCell>
+//             <TableCell>주문 일시</TableCell>
+//             <TableCell>주문 번호</TableCell>
+//             <TableCell>주문 상품</TableCell>
+//             <TableCell>판매자</TableCell>
+//             <TableCell>주문자(아이디)</TableCell>
+//             <TableCell>결제 금액</TableCell>
+//             <TableCell>결제 상태</TableCell>
+//           </TableRow>
+//         </TableHead>
+//         <TableBody>
+//           {rows.map((row) => (
+//             <TableRow key={row.id}>
+//               <TableCell>{row.check}</TableCell>
+//               <TableCell>{row.id}</TableCell>
+//               <TableCell>{row.type}</TableCell>
+//               <TableCell>{row.date}</TableCell>
+//               <TableCell>{row.num}</TableCell>
+//               <TableCell>{row.product}</TableCell>
+//               <TableCell>{row.seller}</TableCell>
+//               <TableCell>{row.orderer}</TableCell>
+//               <TableCell>{row.price}</TableCell>
+//               <TableCell>{row.status}</TableCell>
+              
+//             </TableRow>
+//           ))}
+//         </TableBody>
+//       </Table>
+//       <div className={classes.page}>
+//         <PageControl />
+//       </div>
+      
+//       <div className={classes.seeMore}>
+//         <Link color="primary" href="#" onClick={preventDefault}>
+//           See more orders
+//         </Link>
+//       </div>
+//     </React.Fragment>
+//   );
+// }
+import React, {useEffect} from 'react';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -10,6 +107,7 @@ import Title from './Title';
 import RowList from './RowList.js';
 import Search from './Search';
 import PageControl from './PageControl';
+import axios from 'axios';
 // Generate Order Data
 function createData(check, id, type, date , num, product, seller, orderer,price, status) {
   return { check, id, type, date , num, product, seller, orderer, price, status };
@@ -43,13 +141,26 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Orders() {
   const classes = useStyles();
+
+  useEffect(() => {
+    axios.get('/api/order')
+    .then(response => {
+      alert(response)
+      console.log(response)
+      // if(response.data.success) {
+      //   alert(response.data)
+      // }else {
+      //   alert('실패')
+      // }
+    })
+  },[])
+
   return (
     <React.Fragment >
       <Title >주문 현황</Title>
       <Search />
       <RowList/>
 
-      
       <Table size="small">
         <TableHead className={classes.tablehead}>
           <TableRow>
