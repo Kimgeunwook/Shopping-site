37cec23ae4133a7518d9f21f4af55f3ba3a32709
geunwook <wook961206@naver.com>
2020-08-19 16:00:37 +0900

    게시판 테이블완료

--- servers/routes/noticeBoard.js
+++ servers/routes/noticeBoard.js
@@ -18,5 +18,13 @@ module.exports = function(app, NoticeBoard){//함수로 만들어 객체 app을 
         })
     })
     
+    router.get('/table', function (req, res) {
+        NoticeBoard.find().populate('writer').skip((req.query.page - 1) * 10).limit(10)
+        .then(ord =>{
+            console.log(ord)
+            res.send(ord)
+        })
+
+	})
     return router;	//라우터를 리턴
 };
--- src/NoticeBoard.js
+++ src/NoticeBoard.js
@@ -1,35 +1,99 @@
-import React from 'react';
+import React, {useState , useEffect} from 'react';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Title from './Title';
-
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import RowList from './RowList.js';
+import Search from './Search';
+import PageControl from './PageControl';
+import {useHistory} from 'react-router-dom';
+import axios from 'axios';
 function preventDefault(event) {
   event.preventDefault();
 }
 
-const useStyles = makeStyles({
-  depositContext: {
-    flex: 1,
+const useStyles = makeStyles((theme) => ({
+  seeMore: {
+    marginTop: theme.spacing(3),
   },
-});
+  tablehead : {
+    background : 'lightgray',
+  },
+  page : {
+    display: 'flex',
+    marginTop : theme.spacing(3),
+    justifyContent: 'center',
+  },
+  result: {
+    display: 'flex',
+    marginTop : theme.spacing(3),
+    justifyContent: 'flex-end',
+  }, 
+  button : {
+    marginTop : theme.spacing(2),
+    textAlign: 'right',
+  },
+  buttonelement : {
+    marginRight : theme.spacing(3),
+  },
+}));
 
 export default function Deposits() {
   const classes = useStyles();
+  const history = useHistory();
+  const [NoticeBoard, setNoticeBoard] = useState([])
+  const [page, setPage] = useState(1)
+  const [keyword, setkeyword] = useState('')
+  const [keyText, setkeyText] = useState('')
+  const keywordArr = [['type','분류'],['title', '제목'],['writer', '글쓴이']]
+  useEffect(() => {
+    axios.get(`/api/noticeBoard/table?page=${page}`)
+    .then(response => {
+      setNoticeBoard(response.data)
+    })
+  },[])
+
+  useEffect(() => {
+    console.log(NoticeBoard)
+  },[NoticeBoard])
   return (
-    <React.Fragment>
-      <Title>Recent Deposits</Title>
-      <Typography component="span" variant="h4">
-        $3,024.00
-      </Typography>
-      <Typography color="textSecondary" className={classes.depositContext}>
-        on 15 March, 2019
-      </Typography>
-      <div>
-        <Link color="primary" href="#" onClick={preventDefault}>
-          View balance
-        </Link>
+    <React.Fragment >      
+    <Title >게시판</Title>
+      <Table size="small">
+        <TableHead className={classes.tablehead}>
+          <TableRow>
+            <TableCell>분류</TableCell>
+            <TableCell>제목</TableCell>
+            <TableCell>글쓴이</TableCell>
+            <TableCell>작성일</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {NoticeBoard.map((notice) => (
+            <TableRow key={notice._id}>
+              <TableCell>{notice.type}</TableCell>
+              <TableCell><a>{notice.title}</a></TableCell>
+              <TableCell>{notice.writer.Name}</TableCell>
+              <TableCell>{notice.writeDate.substr(0,10)}</TableCell>    
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className={classes.button}>
+            <Button type="button" onClick = {() => history.push('/App/NoticeAdd')}  className={classes.buttonelement}  variant="outlined" color="primary" >
+                    게시글 추가
+            </Button>         
+        </div>
+      <div className={classes.page}>
+        <PageControl func = {setPage}/>
       </div>
+      
     </React.Fragment>
   );
 }
